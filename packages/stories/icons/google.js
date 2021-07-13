ec56ee4ca2b8f4b7037215a5d1739cf98fef3492
RickyACC <rickyhobbs@acc.edu.au>
2021-07-13 10:44:58 +1000

    working through this Vomit! Eugh

---
+++ packages/stories/icons/google.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Reviews extends React.Component {
+
+  render() {
+    return(
+      <div>
+        <svg width="36.195" height="36.985" viewBox="0 0 36.195 36.985">
+          <g id="Icons" transform="translate(-0.214 -0.533)">
+            <g id="Color-" transform="translate(0.214 0.533)">
+              <g id="Google">
+                <path id="Fill-1" d="M7.789,21.8a10.932,10.932,0,0,1,.556-3.433L2.112,13.6a18.623,18.623,0,0,0,0,16.378l6.229-4.768a10.93,10.93,0,0,1-.55-3.418" transform="translate(-0.214 -3.304)" fill="#fdab00" fillRule="evenodd"/>
+                <path id="Fill-2" d="M19.243,8.1a10.715,10.715,0,0,1,6.818,2.438l5.387-5.38A18.507,18.507,0,0,0,2.623,10.833l6.233,4.76A10.914,10.914,0,0,1,19.243,8.1" transform="translate(-0.725 -0.533)" fill="#e22a26" fillRule="evenodd"/>
+                <path id="Fill-3" d="M19.243,35.851A10.914,10.914,0,0,1,8.856,28.356L2.623,33.115a18.464,18.464,0,0,0,16.62,10.3,17.65,17.65,0,0,0,12.066-4.609l-5.916-4.574a11.451,11.451,0,0,1-6.15,1.618" transform="translate(-0.725 -6.431)" fill="#3d993d" fillRule="evenodd"/>
+                <path id="Fill-4" d="M41.39,23.1a15.3,15.3,0,0,0-.421-3.363H23.714v7.145h9.932a8.328,8.328,0,0,1-3.783,5.527l5.916,4.574c3.4-3.155,5.611-7.856,5.611-13.883" transform="translate(-5.195 -4.603)" fill="#346bb3" fillRule="evenodd"/>
+              </g>
+            </g>
+          </g>
+        </svg>
+
+      </div>
+    );
+  }
+}
+
+export default Reviews;
---
+++ packages/stories/icons/star.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Reviews extends React.Component {
+
+  render() {
+    return(
+      <div>
+        <svg width="26.079" height="23.907" viewBox="0 0 26.079 23.907">
+          <path id="Path_49" data-name="Path 49" d="M1.6-198.051l-4.077-7.477-4.079,7.477-8.962,1.377,6.832,6.153-2.052,8.9,8.257-4.128,8.133,4.128-1.928-8.9,6.838-6.155Z" transform="translate(15.517 205.528)" fill="#ffcf34"/>
+        </svg>
+      </div>
+    );
+  }
+}
+
+export default Reviews;
