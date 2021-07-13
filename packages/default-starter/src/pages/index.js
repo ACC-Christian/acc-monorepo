f7a9096dc4ec55261fdc373fa68d568d9719ccd7
RickyACC <rickyhobbs@acc.edu.au>
2021-07-13 10:44:33 +1000

    working through this Vomit! Eugh

--- packages/default-starter/src/pages/index.js
+++ packages/default-starter/src/pages/index.js
@@ -2,6 +2,13 @@ import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
+import { Button } from "stories/button/Button";
+
+import "fontsource-montserrat/300.css" // All styles included.
+import "fontsource-montserrat/400.css" // All styles included.
+import "fontsource-montserrat/500.css" // All styles included.
+import "fontsource-montserrat/700.css" // All styles included.
+
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
@@ -23,6 +30,7 @@ const IndexPage = () => (
       style={{ marginBottom: `1.45rem` }}
     />
     <p>
+        <Button />
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     </p>
---
+++ packages/stories/acclogo/AccLogo.stories.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+
+import AccLogo from "./accLogo";
+
+export default {
+  title: "Visuals/ACCLOGO",
+  component: Logo
+};
+
+// const Template = args => <AccLogo {...args} />;
+const Template = (args) => <AccLogo {...args} />;
