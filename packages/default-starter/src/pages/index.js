f8610530dcfdffcfb0e17fc1e076f58fb3e7b02b
RickyACC <rickyhobbs@acc.edu.au>
2021-07-15 15:08:45 +1000

    stories now import into pages content now importing into a story via a page

--- packages/default-starter/src/pages/index.js
+++ packages/default-starter/src/pages/index.js
@@ -2,11 +2,8 @@ import React from 'react'
 import { Link, graphql } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 // import get from 'lodash/get'
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
-import { Button } from "stories/button/Button";
-import { AccLogo } from "stories/AccLogo/AccLogo";
-import { Cta } from "stories/cta/Cta"
-import { AcademicImprovement } from "stories/academic-improvement/AcademicImprovement"
 // import Content from "./content.md"
 
 import "fontsource-montserrat/300.css" // All styles included.
@@ -17,12 +14,16 @@ import "fontsource-montserrat/700.css" // All styles included.
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-// const AcademicContent = data.markdownRemark.html
+import { Button } from "stories/button/Button";
+import { AccLogo } from "stories/AccLogo/AccLogo";
+import { Cta } from "stories/cta/Cta"
+import { AcademicImprovement } from "stories/academic-improvement/AcademicImprovement"
 
-// const AcademicContent = this.props.data.allMarkdownRemark.edges.node.fileAbsolutePath
-// const IndexPage = ( args, { data } ) => (
-export default function IndexPage({ data }) {
-    return (
+
+// export default function IndexPage({ data }) {
+const IndexPage = ( { data } ) => (
+// const pic = data.academicImprovementImage.gatsbyImageData
+
         <Layout>
             <Seo title="Home"/>
             <h1>Hi people</h1>
@@ -32,7 +33,7 @@ export default function IndexPage({ data }) {
             <a href={`http://${process.env.OTHERPROJECT_URL}`}>Go to OTHER</a>
 
             <StaticImage
-                src="../images/gatsby-astronaut.png"
+                src="../content/images/gatsby-astronaut.png"
                 width={300}
                 quality={95}
                 formats={["AUTO", "WEBP", "AVIF"]}
@@ -40,29 +41,31 @@ export default function IndexPage({ data }) {
                 style={{marginBottom: `1.45rem`}}
             />
             <span>
-            <AccLogo color={'magenta'} campus={'ricky'}/>
-            <Button label="this is a button"/>
-            <AcademicImprovement bodyCopy={data.markdownRemark.html}/>
-            <Cta mainText="this is written in the pages" buttonLabel="click here now"/>
-          <Link to="/page-2/">Go to page 2</Link> <br/>
-          <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-        </span>
+                <AccLogo color={'magenta'} campus={'ricky'}/>
+                <Button label="this is a button"/>
+                {/*<AcademicImprovement bodyCopy={data.markdownRemark.html}/>*/}
+                <AcademicImprovement bodyCopy={data.academicImprovementContent.html} sideImage={data.academicImprovementImage.childImageSharp.gatsbyImageData}/>
+                <Cta mainText="this is written in the pages" buttonLabel="click here now"/>
+              <Link to="/page-2/">Go to page 2</Link> <br/>
+              <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+            </span>
         </Layout>
-    )
-}
+)
+
+export default IndexPage
+
 
 
 export const query = graphql`
  query {
-      allMarkdownRemark {
-        edges {
-          node {
-            html
-          }
-        }
-      }
-      markdownRemark {
+      academicImprovementContent: markdownRemark(headings: {elemMatch: {value: {in: "ACADEMIC IMPROVEMENT"}}}) {
         html
+        fileAbsolutePath
+      }
+      academicImprovementImage: file(name: {eq: "academicImprovementPic"}) {
+        childImageSharp {
+          gatsbyImageData(layout: FULL_WIDTH)
+        }
       }
   }
 `
--- packages/stories/academic-improvement/AcademicImprovement.jsx
+++ packages/stories/academic-improvement/AcademicImprovement.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './academicImprovement.css'
 import { Button } from '../button/Button'
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import styled from "styled-components"
 
-export const AcademicImprovement = ({ color, campus, bodyCopy }) => (
+const Columns = styled.div`
+  display: flex;
+  flex-direction: column;
+  
+  @media(min-width: 768px){
+    flex-direction: row;
+  }
+`
+
+export const AcademicImprovement = ({ color, campus, bodyCopy, sideImage }) => (
 <>
     <section id="academic-improvement" className="first-content-block start-cell inner-container">
         <div className="row">
@@ -12,6 +23,7 @@ export const AcademicImprovement = ({ color, campus, bodyCopy }) => (
             </div>
             <div className="col-12 col-lg-6">
                 <div className="side-image">
+                    <GatsbyImage image={sideImage} alt="this is alt text in the academicimrovement " />
                 </div>
             </div>
         </div>
